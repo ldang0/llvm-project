a745906c7e22855617501e0ccdcce5697e618e61
Dang Libin <libin.dang@gmail.com>
2026-08-27 21:56:06 +0800

    [MMIX] Define LLVM libc target properties

--- libc/src/__support/macros/properties/architectures.h
+++ libc/src/__support/macros/properties/architectures.h
@@ -50,6 +50,10 @@
 #define LIBC_TARGET_ARCH_IS_WASM
 #endif
 
+#if defined(__mmix__)
+#define LIBC_TARGET_ARCH_IS_MMIX
+#endif
+
 #if defined(__aarch64__) || defined(__arm64__) || defined(_M_ARM64)
 #define LIBC_TARGET_ARCH_IS_AARCH64
 #endif
